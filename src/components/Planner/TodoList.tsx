064600a0a29480c972926254a7ba2cccca75903a
veganedge <scottxbrown@yahoo.com>
2023-06-19 15:48:04 -0700

    added MUI to TodoList.jsx

--- src/components/Planner/TodoList.tsx
+++ src/components/Planner/TodoList.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import { Box, IconButton, List, ListItem, ListItemText } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 interface TodoListProps {
   items: {id: string, text: string}[];
   onDeleteTodo: (id: string) => void;
 };
 
-const TodoList: React.FC<TodoListProps> = (props) => {
+const TodoList: React.FC<TodoListProps> = ({ items, onDeleteTodo }) => (
   
-  return (
-    <ul>
-      {props.items.map(todo => 
-        <li key={todo.id}>
-          <span>{todo.text}</span>
-          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>DELETE</button>
-        </li>
-      )}
-    </ul>
-  );
-}
+  <List>
+    {items.map(todo => (
+      <Box key={todo.id}>
+        <Box sx={{ display: 'inline-flex' }}>
+          <ListItem sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <ListItemText primary={todo.text} />
+            <IconButton
+              aria-label="delete"
+              color="error"
+              onClick={() => onDeleteTodo(todo.id)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </ListItem>
+        </Box>
+      </Box>
+    ))}
+  </List>
+)      
 
 export default TodoList;
